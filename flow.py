01f44d3f7bc9d8f6bd9a92c6a6057e68bda18c8c
seva@sevik.org <seva@sevik.org>
2012-02-29 00:06:01 +0200

    flow rate calculation script according to nopheads post

---
+++ flow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import math
+
+h = 0.33
+feed = 10
+
+w = h * 1.5
+
+d_infill = h * 1.31
+s_infill = d_infill * d_infill * math.pi/4
+
+d_fil = 1.75
+s_fil = d_fil * d_fil * math.pi/4
+
+head_len = 3200/104.0
+
+rpm = s_infill / s_fil * feed / head_len * 60
+print rpm
